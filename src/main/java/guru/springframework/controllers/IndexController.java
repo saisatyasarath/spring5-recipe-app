42c5e7aa9c0a8f49a0ec181ec1b5033a021e235b
saisatyasarath <sarathchandhra365@gmail.com>
2022-11-13 22:36:50 +0530

    added table description

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,13 +1,39 @@
 package guru.springframework.controllers;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.UnitOfMeasureRepository;
+
+import guru.springframework.domain.Category;
+import guru.springframework.domain.UnitOfMeasure;
 
 @Controller
 public class IndexController {
 	
+	private CategoryRepository categoryRepository;
+	private UnitOfMeasureRepository unitOfMeasureRepository;
+	
+	public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+	
+		this.categoryRepository = categoryRepository;
+		this.unitOfMeasureRepository = unitOfMeasureRepository;
+	}
+
+
+
 	@RequestMapping({"","/","/index"})
 	public String getIndexPage() {
+		
+		Optional<Category> catrgoryOptional = categoryRepository.findByDescription("American");
+		Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+		
+		System.out.println("Cat Id is " + catrgoryOptional.get().getId());
+		System.out.println("UOM ID is " + unitOfMeasureOptional.get().getId());
+		
 		return "index";
 	}
 
